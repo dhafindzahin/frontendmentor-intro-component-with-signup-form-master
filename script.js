4a5b20661d2ee27271c26cc3f1b20a51a4a8ac72
dhafin <dzahindafhin@gmail.com>
2024-01-17 16:11:25 +0800

    refactor: make it easier to make variety of warning

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
-const formInput = [...document.getElementsByTagName("input")];
+const inputField = [...document.getElementsByClassName("inputField")];
 const submitBtn = document.getElementById("submitBtn");
 
 function emailValidity(userEmail) {
-	return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(userEmail) && userEmail !== "";
+	return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(userEmail);
+}
+
+function formatInputName(string) {
+	return string.charAt(0).toUpperCase() + string.slice(1).replace("-", " ");
 }
 
 submitBtn.addEventListener("click", (e) => {
-	formInput.forEach((input) => {
+	inputField.forEach((inputField) => {
+		const input = inputField.getElementsByTagName("input")[0];
+		const warning = inputField.getElementsByTagName("em")[0];
+
 		if (!input.checkValidity() && input.type !== "email") {
 			input.classList.add("invalid");
-		} else if (input.type === "email" && !emailValidity(input.value)) {
-			input.classList.add("invalid");
+			warning.innerText = formatInputName(input.name) + " cannot be empty";
 		}
+
+		if (input.type === "email" && !emailValidity(input.value)) {
+			input.classList.add("invalid");
+			warning.innerText =
+				"Look like this not an " + formatInputName(input.name);
+			if (input.value === "") {
+				warning.innerText = formatInputName(input.name) + " cannot be empty";
+			}
+		}
+
 		if (input.classList.contains("invalid")) {
 			e.preventDefault();
 		}
 	});
 });
 
-formInput.forEach((input) => {
+inputField.forEach((inputField) => {
+	const input = inputField.getElementsByTagName("input")[0];
+	const warning = inputField.getElementsByTagName("em")[0];
 	input.addEventListener("input", () => {
 		if (input.checkValidity() && input.type !== "email") {
 			input.classList.remove("invalid");
