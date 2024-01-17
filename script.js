b4c0724881c630cad9e0774733df82024198b7ec
dhafin <dzahindafhin@gmail.com>
2024-01-17 13:46:03 +0800

    feat: add costume validator for email input

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const formInput = [...document.getElementsByTagName("input")];
 const submitBtn = document.getElementById("submitBtn");
 
+function checkEmail(userEmail) {
+	return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(userEmail) || userEmail === "";
+}
+
 submitBtn.addEventListener("click", () => {
 	formInput.forEach((input) => {
-		if (input.checkValidity() === false) {
+		if (!input.checkValidity() && input.type !== "email") {
+			input.classList.add("invalid");
+		} else if (input.type === "email" && checkEmail(input.value)) {
 			input.classList.add("invalid");
 		}
 	});
@@ -11,7 +17,9 @@ submitBtn.addEventListener("click", () => {
 
 formInput.forEach((input) => {
 	input.addEventListener("input", () => {
-		if (input.checkValidity() === true) {
+		if (input.checkValidity() && input.type !== "email") {
+			input.classList.remove("invalid");
+		} else if (input.type === "email" && checkEmail(input.value)) {
 			input.classList.remove("invalid");
 		}
 	});
