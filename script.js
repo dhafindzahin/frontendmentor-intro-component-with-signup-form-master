5a846b3559356507195d0666d984fcb39487d6fc
Dhafin Dzahin <dzahindhafin@gmail.com>
2024-01-18 19:05:18 +0800

    feat: make warning show up on submit

--- script.js
+++ script.js
@@ -7,19 +7,38 @@ function formatInputName(string) {
 
 submitBtn.addEventListener("click", (e) => {
 	let checkOnce = true;
-	if (input.classList.contains("invalid")) {
-		e.preventDefault();
-		if (checkOnce) {
-			checkOnce = false;
-			input.focus();
+
+	inputField.forEach((inputField) => {
+		const input = inputField.getElementsByTagName("input")[0];
+
+		const warning = inputField.getElementsByTagName("em")[0];
+		const inputName = formatInputName(input.name);
+		if (!input.checkValidity()) input.classList.add("invalid");
+
+		if (input.validity.valueMissing) {
+			warning.innerText = inputName + " cannot be empty";
 		}
-	}
+		if (input.validity.patternMismatch) {
+			warning.innerText = "Look like this is not an " + inputName;
+		}
+		if (input.validity.tooShort) {
+			warning.innerText = inputName + " need to be longer than " + input.minLength + " character";
+		}
+		if (input.classList.contains("invalid")) {
+			e.preventDefault();
+			if (checkOnce) {
+				checkOnce = false;
+				input.focus();
+			}
+		}
+	});
 });
 inputField.forEach((inputField) => {
 	const input = inputField.getElementsByTagName("input")[0];
-	const warning = inputField.getElementsByTagName("em")[0];
-	const inputName = formatInputName(input.name);
+
 	input.addEventListener("blur", () => {
+		const warning = inputField.getElementsByTagName("em")[0];
+		const inputName = formatInputName(input.name);
 		if (!input.checkValidity()) input.classList.add("invalid");
 
 		if (input.validity.valueMissing) {
